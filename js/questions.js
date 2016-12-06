db09e9f5f209220158d6b2ac007eec2b0feb5eb5
user.name <user.email>
2016-12-06 11:50:16 +0000

    Add correct round questions and bonus answer

--- js/questions.js
+++ js/questions.js
@@ -1,19 +1,22 @@
 var Q1 = {
-	title: "Who is jesus",
-	answers: ["one", "two", "three", "four", "five"],
-	scores: [1, 2, 3, 4, 5]
+	title: "In the news today _____ hacks the UK government.",
+	answers: ["Russia", "Teenager", "China", "Anonymous", "ISIS", "Wikileaks"],
+	scores: [38, 25, 16, 12, 6, 3],
+    bonus: "ECORP"
 }
 
 var Q2 = {
-	title: "is he your lord",
-	answers: ["yes", "yes", "yes"],
-	scores: [1, 2, 3, 4, 5]
+	title: "Name an antivirus.",
+	answers: ["Norton", "McAfee", "AVG", "Avast", "Bullguard", "360 security"],
+	scores: [40, 26, 14, 11, 7, 3],
+    bonus: "antibiotic"
 }
 
 var Q3 = {
-	title: "or saviour",
-	answers: ["yes", "yes"],
-	scores: [1, 2, 3, 4, 5]
+	title: "Name a high profile company that has been hacked.",
+	answers: ["Ashley Madison", "Yahoo", "Wikileaks", "Tesco Bank", "Apple", "Talk Talk", "Sony"],
+	scores: [24, 20, 20, 14, 9, 8, 5],
+    bonus: "Wikipedia"
 }
 
 var Questions = [Q1, Q2, Q3];
